a3125888d65dbfd74498948e20999d0b4ef04b81
raveeshpanwar <63387902+raveeshpanwar@users.noreply.github.com>
2022-02-15 15:49:58 +0530

    More responsiveness

--- index.js
+++ index.js
@@ -10,8 +10,3 @@ function clicko()
 }
 }
 let sounds=['sounds/crash.mp3','sounds/kick-bass.mp3','sounds/snare.mp3','sounds/tom-1.mp3','sounds/tom-2.mp3','sounds/tom-3.mp3','sounds/tom-4.mp3'];
-/*let drums=['','images/kick-bass.png','images/snare.png','images/tom-1.png','images/tom-2.png','images/tom-3.png','images/tom-4.png'];
-
-for(let z=0;z<7;z++){
-   document.getElementsByTagName('button')[z].style.backgroundImage=url(drums[z]);
-   }*/
