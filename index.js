c0c08b3b58099c3619981f11e536bdf7472fb1c4
raveeshpanwar <63387902+raveeshpanwar@users.noreply.github.com>
2022-02-16 18:48:04 +0530

    Keyboard Audio support
    Play audio by keystrokes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+let sounds=['sounds/crash.mp3','sounds/kick-bass.mp3','sounds/snare.mp3','sounds/tom-1.mp3','sounds/tom-2.mp3','sounds/tom-3.mp3','sounds/tom-4.mp3'];
+let keykey=['w','a','s','d','j','k','l'];
 
+//Audio on clicking buttons
 for(let x=0;x<7;x++)
 {
 document.getElementsByTagName('button')[x].addEventListener('click',clicko);
@@ -6,7 +9,15 @@ function clicko()
 {
    let a=new Audio(sounds[x]);
    a.play();
+   document.getElementsByTagName('button')[x].classList.toggle('white');
+}
+}
 
+//Audio on Pressing keys
+document.addEventListener('keydown',(event)=>{makeSound(event.key)});
+function makeSound(n){for(let i=0;i<7;i++){
+if(n==keykey[i]){
+   let a=new Audio(sounds[i]);
+   a.play(); document.getElementsByTagName('button')[i].classList.toggle('white');
 }
-}
-let sounds=['sounds/crash.mp3','sounds/kick-bass.mp3','sounds/snare.mp3','sounds/tom-1.mp3','sounds/tom-2.mp3','sounds/tom-3.mp3','sounds/tom-4.mp3'];
+}}
